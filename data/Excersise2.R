a46afe13bdf5235de2b16128f278a591eda44dee
Askarolla <theodor.petrik@mensa.cz>
2019-11-10 20:41:14 +0200

    FInished data wrangling

--- data/Excersise2.R
+++ data/Excersise2.R
@@ -1,2 +1,51 @@
 # Theodor Petřík, November 10, 2019. Regression and model validation.
 
+#packages
+install.packages("gdata")
+library(gdata)
+library(dplyr)
+#getting the data
+url <-"http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt"
+download.file(url, destfile = "datal")
+data <- read.table("datal", header = TRUE, sep="\t", stringsAsFactors = TRUE)
+
+#wrangling
+dim(data)
+str(data)
+
+#there are 183 observations of 60 variables. The variables are all int except for the last one which is factor (gender)
+
+#making the analysis data set
+# creating new vars and scaling them
+d_sm <- data$D03 + data$D11+ data$D19 + data$D27
+d_ri <- data$D07 + data$D14 + data$D22 + data$D30
+d_ue <- data$D06 + data$D15 + data$D23 + data$D31
+deep <- (d_sm + d_ri + d_ue)/12
+
+su_lp <- data$SU02 + data$SU10+ data$SU18 + data$SU26
+su_um <- data$SU05 + data$SU13 + data$SU21 + data$SU29
+su_sb <- data$SU08 + data$SU16 + data$SU24 + data$SU32
+surf <- (su_lp + su_um + su_sb)/12
+
+st_os <- data$ST01 + data$ST09+ data$ST17 + data$ST25
+st_tm <- data$ST04 + data$ST12 + data$ST20 + data$ST28
+stra <- (st_os + st_tm)/8
+
+#making the new set and filtering out observations where point == 0
+analysis_data <- data.frame(data$gender, data$Age, data$Attitude, deep, stra, surf, data$Points)
+
+#renaming columns
+names(analysis_data)[1] <- "gender"
+names(analysis_data)[2] <- "age"
+names(analysis_data)[3] <- "attitude"
+names(analysis_data)[7] <- "points"
+
+analysis_data <-analysis_data %>%
+  dplyr::filter(points != 0)
+
+#saving the dataset
+write.csv(analysis_data, file = "learning2014.csv", row.names = FALSE)
+
+#testing that it works
+test <- read.csv("learning2014.csv")
+str(test)
